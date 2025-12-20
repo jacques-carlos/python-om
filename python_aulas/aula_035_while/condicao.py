b1d05afbba02544ec06db0e83ee4c0dd055ef066
Jacques Carlos <jacquescarlos05@gmail.com>
2025-12-20 16:45:02 -0300

    aula34 - aula36

---
+++ python_aulas/aula_035_while/condicao.py
@@ -0,0 +1,7 @@
+contador = 0
+
+while contador <= 100:
+    print(contador, end=' ')
+    contador += 1
+
+print('Acabou.')
---
+++ python_aulas/aula_036_op_atribuicao/main.py
@@ -0,0 +1,14 @@
+"""
+
+Operadores de atribuição
+= += -= *= /= //= **= %=
+
+Operações matemáticas, concatenação, repetição
+
+"""
+
+contador = 10
+
+contador += 5
+
+print(contador)
