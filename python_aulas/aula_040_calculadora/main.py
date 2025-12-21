bb3f6bca153ded5b6c133905be74fbba1def739b
Jacques Carlos <jacquescarlos05@gmail.com>
2025-12-20 23:35:11 -0300

    exercicio update

--- python_aulas/aula_040_calculadora/main.py
+++ python_aulas/aula_040_calculadora/main.py
@@ -1,9 +1,5 @@
 # CALCULADORA COM WHILE
 
-#  variáveis:
-entrada_1 = entrada_2 = operador = sair = ''  # str
-valor_1 = valor_2 = resultado = 0  # int
-
 while True:
     resultado = 0  # o resultado sempre zera a cada repetição
     sair = 'Sem resposta'  # a resposta sempre "zera" também
@@ -24,38 +20,44 @@ while True:
         print('Erro! Digite valores inteiros.')
 
     else:
-        # se o operador for "+" os resultados somam-se
-        if operador == '+':
-            resultado = valor_1 + valor_2
-            # retornar o resultado
-            print(f'{entrada_1} {operador} {entrada_2} = {resultado}')
+        # analisa se o usuário informou mais de um operador
+        if len(operador) > 1:
+            print('Erro! Digite apenas um operador.')
 
-        # se o operador for "-" os resultados subtraem-se
-        elif operador == '-':
-            resultado = valor_1 - valor_2
-            # retornar o resultado
-            print(f'{entrada_1} {operador} {entrada_2} = {resultado}')
-
-        # se o operador for "*" os resultados multiplicam-se
-        elif operador == 'x' or operador == '*':
-            resultado = valor_1 * valor_2
-            # retornar o resultado
-            print(f'{entrada_1} {operador} {entrada_2} = {resultado}')
-
-        # se o operador for "/" os resultados dividem-se
-        elif operador == '/':
-            resultado = valor_1 / valor_2
-            # retornar o resultado com duas casas decimais
-            print(f'{entrada_1} {operador} {entrada_2} = {resultado:.2f}')
-
-        # caso o usuário digite qualquer coisa que não seja um operador
         else:
-            print('Erro! Digite um operador válido.')
+            # se o operador for "+" os resultados somam-se
+            if operador == '+':
+                resultado = valor_1 + valor_2
+                # retornar o resultado
+                print(f'{entrada_1} {operador} {entrada_2} = {resultado}')
+
+            # se o operador for "-" os resultados subtraem-se
+            elif operador == '-':
+                resultado = valor_1 - valor_2
+                # retornar o resultado
+                print(f'{entrada_1} {operador} {entrada_2} = {resultado}')
+
+            # se o operador for "*" os resultados multiplicam-se
+            elif operador == 'x' or operador == '*':
+                resultado = valor_1 * valor_2
+                # retornar o resultado
+                print(f'{entrada_1} {operador} {entrada_2} = {resultado}')
+
+            # se o operador for "/" os resultados dividem-se
+            elif operador == '/':
+                resultado = valor_1 / valor_2
+                # retornar o resultado com duas casas decimais
+                print(f'{entrada_1} {operador} {entrada_2} = {resultado:.2f}')
+
+            # caso o usuário digite qualquer coisa que não seja um operador
+            else:
+                print('Erro! Digite um operador válido.')
 
     finally:
         # pergunta se o usuário deseja sair do programa
-        while sair not in 'SsNn':
-            sair = input('Deseja sair da calculadora? [S/N]: ').strip()
-        if sair in 'Ss':
+        while sair not in 'sn':  # força o usuário a responder validamente
+            sair = input('Deseja sair da calculadora? [s/n]: ')
+            sair = sair.strip().lower()[0]
+        if sair is 's':  # se a reposta for positiva, sai do programa
             print('Encerrando o programa... Até mais!')
             break
